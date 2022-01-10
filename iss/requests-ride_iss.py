c45382e7c7e44150be0abf468d0333d1c0787e8c
rian <rianBoren@users.noreply.github.com>
2022-01-10 22:17:06 +0000

    pulling in data from API with python standard lib

---
+++ iss/requests-ride_iss.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+tracking the iss using
+api.open-notify.org/astros.json
+"""
+
+
+import requests
+
+# Define URL
+MAJORTOM = 'http://api.open-notify.org/astros.json'
+
+
+def main():
+    """runtime code"""
+
+    # Call the webservice
+    groundctrl = requests.get(MAJORTOM)
+    # send a post with requests.post()
+    # send a put with requests.put()
+    # send a delete with requests.delete()
+    # send a head with requests.head()
+
+    # strip the json off the 200 that was returned by our API
+    # translate the json into python lists and dictionaries
+    helmetson = groundctrl.json()
+
+    # display our Pythonic data
+    print("\n\nConverted Python data:")
+    print(helmetson)
+
+    print('\n\nPeople in Space: ', helmetson['number'])
+    people = helmetson['people']
+    print(people)
+
+    # data grabbing tweaks
+    print('\nhuzzah:')
+    print(f"people in space: {helmetson['number']}")
+    for astronaut in helmetson['people']:
+        print(f"{astronaut['name']} on the {astronaut['craft']}")
+
+
+if __name__ == "__main__":
+    main()
+
